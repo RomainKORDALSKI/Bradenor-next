89238d80b4c322d90034ea826fa71562becb4812
RomainKORDALSKI <romain.kordalski@oclock.school>
2024-10-09 08:58:25 +0200

    fix leafeat

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
-import { FiMenu, FiX, FiUser, FiLogOut } from 'react-icons/fi';
-import { useRouter } from 'next/router';
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { FiMenu, FiX, FiUser, FiLogOut } from "react-icons/fi";
+import { useRouter } from "next/router";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -9,13 +9,13 @@ const Header = () => {
   const router = useRouter();
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       try {
-        const decodedToken = JSON.parse(atob(token.split('.')[1]));
+        const decodedToken = JSON.parse(atob(token.split(".")[1]));
         setUser(decodedToken);
       } catch (error) {
-        console.error('Erreur lors du décodage du token:', error);
+        console.error("Erreur lors du décodage du token:", error);
       }
     }
   }, []);
@@ -25,33 +25,47 @@ const Header = () => {
   };
 
   const handleLogout = () => {
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     setUser(null);
-    router.push('/');
+    router.push("/");
   };
 
   return (
     <header className="header">
-  <div className="header__left">
-    <button className="header__menu-toggle" onClick={toggleMenu}>
-      {menuOpen ? <FiX /> : <FiMenu />}
-    </button>
-    <ul className={`header__menu ${menuOpen ? 'open' : ''}`}>
-      <li><Link href="/">Accueil</Link></li>
-      {!user ? (
-        <>
-        <li><Link href="/user/login">Se connecter</Link></li>
-        <li><Link href="/user/register">Créer un compte</Link></li>
-        </>
-      ) : (
-        <>
-          <li><Link href="/user/formulaire">Créer un événenement</Link></li>
-          <li><a href="#" onClick={handleLogout}>Déconnexion</a></li>
-        </>
-      )}
-      <li><Link href="#">Contact</Link></li>
-    </ul>
-  </div>
+      <div className="header__left">
+        <button className="header__menu-toggle" onClick={toggleMenu}>
+          {menuOpen ? <FiX /> : <FiMenu />}
+        </button>
+        <ul className={`header__menu ${menuOpen ? "open" : ""}`}>
+          <li>
+            <Link href="/">Accueil</Link>
+          </li>
+          {!user ? (
+            <>
+              <li>
+                <Link href="/user/login">Se connecter</Link>
+              </li>
+              <li>
+                <Link href="/user/register">Créer un compte</Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link href="/user/formulaire">Créer un événenement</Link>
+              </li>
+              <li>
+                <a href="#" onClick={handleLogout}>
+                  Déconnexion
+                </a>
+              </li>
+            </>
+          )}
+          <li>
+            <Link href="#">Contact</Link>
+          </li>
+        </ul>
+      </div>
       <Link className="header__a" href="/">
         <div className="header__middle">
           <h1>BradEnOr</h1>
@@ -60,12 +74,17 @@ const Header = () => {
       <div className="header__right">
         {!user ? (
           <button className="header__login-button cta-button">
-            <Link href="/user/login"><FiUser /></Link>
+            <Link href="/user/login">
+              <FiUser />
+            </Link>
           </button>
         ) : (
           <div className="header__user-info">
             <span>Bonjour, {user.prenom}</span>
-            <button className="header__logout-button cta-button" onClick={handleLogout}>
+            <button
+              className="header__logout-button cta-button"
+              onClick={handleLogout}
+            >
               <FiLogOut />
             </button>
           </div>
@@ -76,4 +95,3 @@ const Header = () => {
 };
 
 export default Header;
-
