8123f01725e15e3037b2baaba8a22eb8348a4f14
RomainKORDALSKI <romain.kordalski@oclock.school>
2024-10-13 22:04:14 +0200

    search tw

--- src/app/components/header/Bookmark.js
+++ src/app/components/header/Bookmark.js
@@ -1,22 +1,21 @@
-import React, { useState, useEffect, useContext } from 'react';
-import axios from 'axios';
-import EventList from '@/app/components/EventList';
-import { FavoritesContext } from '@/app/context/FavoritesContext';
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import EventList from "@/app/components/EventList";
+import { FavoritesContext } from "@/app/context/FavoritesContext";
 
 const Bookmark = ({ closeModal }) => {
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(false);
   const { favorites, removeFavorite } = useContext(FavoritesContext);
 
-
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const response = await fetch('/api/events/allevents');
+        const response = await fetch("/api/events/allevents");
         const data = await response.json();
         setEvents(data);
       } catch (error) {
-        console.error('Error fetching events', error);
+        console.error("Error fetching events", error);
       }
     };
 
@@ -35,24 +34,23 @@ const Bookmark = ({ closeModal }) => {
   const removeFavoriteFromFavorites = async (eventId) => {
     try {
       setLoading(true);
-      await axios.post('/api/favorites/remove', { id: eventId });
+      await axios.post("/api/favorites/remove", { id: eventId });
       removeFavorite(eventId);
     } catch (error) {
-      console.error('Error removing favorite', error);
+      console.error("Error removing favorite", error);
     } finally {
       setLoading(false);
     }
   };
 
-
   return (
     <div className="favorite-events">
       <h2>Mes Favoris :</h2>
       {favorites.length === 0 ? (
-        <p className='favorite-p'>Aucun favori pour le moment.</p>
+        <p className="favorite-p">Aucun favori pour le moment.</p>
       ) : (
         <EventList
-          events={events.filter(event => favorites.includes(event.id))}
+          events={events.filter((event) => favorites.includes(event.id))}
           grouped={false}
           showCity={true}
           showDetails={false}
@@ -67,16 +65,3 @@ const Bookmark = ({ closeModal }) => {
 };
 
 export default Bookmark;
-
-
-
-
-
-
-
-
-
-
-
-
-
